5fa6c9ec03c04c0b265eb19b92390fddcb636458
mao-test-h <miymao78.d@gmail.com>
2025-09-28 19:46:03 +0900

    feat: 動画再生テスト

--- Assets/_Example/Runtime/ExampleApplication.cs
+++ Assets/_Example/Runtime/ExampleApplication.cs
@@ -1,61 +1,83 @@
 using System;
 using LifecycleHandler;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace _Example
 {
     internal sealed class ExampleApplication : MonoBehaviour, IViewControllerLifecycleListener
     {
+        [SerializeField] private Button playMovieButton;
+
+        private string Tag => $"[{nameof(ExampleApplication)}]";
         private IDisposable _lifecycleHandler;
 
         private void Awake()
         {
             _lifecycleHandler = ViewControllerLifecycleHandlerFactory.Create(this);
+
+            // イベント確認用にフルスクリーン動画を再生
+            playMovieButton.onClick.AddListener(() =>
+            {
+                const string url = "https://devstreaming-cdn.apple.com/videos/streaming/examples/bipbop_16x9/bipbop_16x9_variant.m3u8";
+                var ret = Handheld.PlayFullScreenMovie(url);
+                Debug.Log($"{Tag} Handheld.PlayFullScreenMovie returned {ret}");
+            });
         }
 
         private void OnDestroy()
         {
-            _lifecycleHandler?.Dispose();
+            _lifecycleHandler.Dispose();
+        }
+
+        void OnApplicationFocus(bool hasFocus)
+        {
+            Debug.Log($"{Tag} >> OnApplicationFocus: " + hasFocus);
+        }
+
+        void OnApplicationPause(bool pauseStatus)
+        {
+            Debug.Log($"{Tag} >> OnApplicationPause: " + pauseStatus);
         }
 
         public void OnViewWillLayoutSubviewsCallbacks()
         {
-            Debug.Log("[ExampleApplication] OnViewWillLayoutSubviews");
+            Debug.Log($"{Tag} OnViewWillLayoutSubviews");
         }
 
         public void OnViewDidLayoutSubviewsCallbacks()
         {
-            Debug.Log("[ExampleApplication] OnViewDidLayoutSubviews");
+            Debug.Log($"{Tag} OnViewDidLayoutSubviews");
         }
 
         public void OnViewWillDisappearCallbacks(bool animated)
         {
-            Debug.Log($"[ExampleApplication] OnViewWillDisappear (animated: {animated})");
+            Debug.Log($"{Tag} OnViewWillDisappear (animated: {animated})");
         }
 
         public void OnViewDidDisappearCallbacks(bool animated)
         {
-            Debug.Log($"[ExampleApplication] OnViewDidDisappear (animated: {animated})");
+            Debug.Log($"{Tag} OnViewDidDisappear (animated: {animated})");
         }
 
         public void OnViewWillAppearCallbacks(bool animated)
         {
-            Debug.Log($"[ExampleApplication] OnViewWillAppear (animated: {animated})");
+            Debug.Log($"{Tag} OnViewWillAppear (animated: {animated})");
         }
 
         public void OnViewDidAppearCallbacks(bool animated)
         {
-            Debug.Log($"[ExampleApplication] OnViewDidAppear (animated: {animated})");
+            Debug.Log($"{Tag} OnViewDidAppear (animated: {animated})");
         }
 
         public void OnInterfaceWillChangeOrientationCallbacks()
         {
-            Debug.Log("[ExampleApplication] OnInterfaceWillChangeOrientation");
+            Debug.Log($"{Tag} OnInterfaceWillChangeOrientation");
         }
 
         public void OnInterfaceDidChangeOrientationCallbacks()
         {
-            Debug.Log("[ExampleApplication] OnInterfaceDidChangeOrientation");
+            Debug.Log($"{Tag} OnInterfaceDidChangeOrientation");
         }
     }
 }
